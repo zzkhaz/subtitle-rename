e48158011de41658961ecd28483d1f82727a8875
Akiyau <250523927@qq.com>
2017-10-25 19:23:20 +0800

    增加了同时修改简体字幕与繁体字幕功能

--- subtitle-rename.py
+++ subtitle-rename.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 import os
+import re
 '''
-使用前请保持待修改的字幕数量与对应的视频数量一致
+使用前请保持待修改的字幕集数与对应的视频数量一致
 '''
 def endsWith(s,*endstring):
         array = map(s.endswith,endstring)
@@ -10,7 +11,7 @@ def endsWith(s,*endstring):
         else:
                 return False
 
-def getFileName(path):
+def getFileName(path,Type):
         namelist = []
         i = 0
         videoNum = 0
@@ -24,17 +25,34 @@ def getFileName(path):
         for filename in os.listdir(path):
                 if endsWith(filename,'.ass','.ssa','.srt'):
                         subtitleNum += 1
-        if videoNum == subtitleNum:
-                for filename in os.listdir('.'):
+        if (videoNum == subtitleNum) and (Type == 'n'):
+                for filename in os.listdir(path):
                         if endsWith(filename,'.ass','.ssa','.srt'):
                                 print (namelist[i] + os.path.splitext(filename)[1])
                                 os.rename(filename, namelist[i] + os.path.splitext(filename)[1])
                                 i += 1
                 print('修改成功！')
+        elif (subtitleNum == videoNum*2) and (Type == 'y'):
+                for filename in os.listdir(path):
+                        subtitleType = re.findall(".*[.](.*)[.].*",filename)
+                        if subtitleType:
+                                print (subtitleType[0])
+                                if ('sc' in subtitleType[0]) or ('chs' in subtitleType[0]):
+                                        if endsWith(filename,'.ass','.ssa','.srt'):
+                                                print (namelist[i] + '.' + subtitleType[0] + os.path.splitext(filename)[1])
+                                                os.rename(filename, namelist[i] + '.' + subtitleType[0] + os.path.splitext(filename)[1])
+                                elif ('tc' in subtitleType[0]) or ('cht' in subtitleType[0]):
+                                        if endsWith(filename,'.ass','.ssa','.srt'):
+                                                i -= 1
+                                                print (namelist[i] + '.' + subtitleType[0] + os.path.splitext(filename)[1])
+                                                os.rename(filename, namelist[i] + '.' + subtitleType[0] + os.path.splitext(filename)[1])
+                                i += 1
+                print('修改成功！')
         else:
-                print('字幕数量与视频数量不匹配，请修改！')
-        
+                print('参数错误，请修改！')
 
 if __name__ == '__main__':
         path = './'
-        getFileName(path)
+#       设置文件路径     
+        Type = input("是否同时含有简体字幕与繁体字幕（y/n）")
+        getFileName(path,Type)
